70ca7eff9c8fb1f1d7c25bd21b46aa74876dbb93
Sarmad Jamal <sarmadjamal2002@gmail.com>
2023-04-21 23:19:30 +0500

    firstVersion

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,11 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import localFont from "next/font/local";
 
-const sharpGrotesk = localFont({
-  src: "/fonts/Sharp_Grotesk/SharpGrotesk-light20.otf",
-  variable: "--font-sharpGrotesk",
-});
+
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <main className={`${sharpGrotesk.variable} font-sans`}>
+    <main >
       <Component {...pageProps} />
     </main>
   );
